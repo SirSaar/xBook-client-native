eda5a3bc0d2a4d8bf867f4438ae7b956ff470d68
SirSaar <saar643@gmail.com>
2019-02-14 16:38:52 +0200

    chore: change things

--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { Image, Button, StyleSheet, Text, View, WebView } from 'react-native';
-import { AuthSession } from 'expo';
 
 
 const LOGIN_URL = "https://glacial-fortress-14735.herokuapp.com/api/user/auth/facebook";
 const SUCCESS_URL = "https://glacial-fortress-14735.herokuapp.com/api/user/profile";
+const FAILED_URL = "https://glacial-fortress-14735.herokuapp.com/api/user/auth/failed";
 
 export default class SignIn extends Component {
-  state = {isLoggedIn: false}
+  state = {isLoggedIn: false, fbSignIn: false}
   render() {
-    if(!this.state.isLoggedIn) {
+    if(this.state.isLoggedIn) return this.props.navigation.navigate('App');
+    if(this.state.fbSignIn) {
+      return (
       <View style={[styles.container]}>
         <WebView
           ref={'webview'}
@@ -22,6 +24,7 @@ export default class SignIn extends Component {
           scalesPageToFit={true}
         />
       </View>
+      )
     } else
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -33,13 +36,13 @@ export default class SignIn extends Component {
   onNavigationStateChange (navState) {
     if (navState.url == SUCCESS_URL) {
       this.setState({
-        loggedIn: true,
+        isLoggedIn: true,
       });
     }
   }
 
-  _handlePress = async () => {
-
+  _handlePress = () => {
+    this.setState({fbSignIn: true})
   }
 }
 
