7158b5e08060ea134519bbc63e1993197bce351c
SirSaar <saar643@gmail.com>
2019-02-18 13:00:00 +0200

    refactor

--- App.js
+++ App.js
@@ -2,10 +2,31 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { AppContainer } from './router';
 
+import { useStrict } from 'mobx';
+import promiseFinally from 'promise.prototype.finally';
+import { Provider } from 'mobx-react';
+
+import userStore from './stores/user.store';
+import authStore from './stores/auth.store';
+
+const stores = {
+  userStore,
+  authStore
+};
+
+// For easier debugging
+window._____APP_STATE_____ = stores;
+
+promiseFinally.shim();
+useStrict(true);
+
+
 export default class App extends React.Component {
   render() {
     return (
-      <AppContainer />
+      <Provider {...stores}>
+        <AppContainer />
+      </Provider>
     );
   }
 }
